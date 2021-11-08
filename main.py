82a2780f2d18a681411ae34e907e0bc18fd86468
jojotrujillo <jordontrujillo@outlook.com>
2021-11-07 18:47:33 -0600

    Add GPIO functionality

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import bluetooth
-import time
-# import Adafruit_BBIO.GPIO as GPIO
+import bluetooth, time
+from gpiozero import LED
 
 search_time = 10
-led_pin = "P8_7"
+led_pin = LED(17)
 # You can hardcode the desired device ID here as a string to skip the discovery stage
 addr = "F4:AF:E7:72:CB:19"
 
-print("Welcome to the Bluetooth Detection Demo! \nMake sure your desired Bluetooth-capable device is turned on and "
-      "discoverable.")
+print("Welcome to the Bluetooth Detection Demo! \nMake sure your desired Bluetooth-capable device is turned on and discoverable.")
 
-if addr is None:
+if addr == None:
     try:
         input("When you are ready to begin, press the Enter key to continue...")
     except SyntaxError:
@@ -26,20 +24,19 @@ if addr is None:
         print("No devices found! Please check your Bluetooth device and restart the demo!")
         exit(0)
 
-    i = 0  # Just an incrementer for labeling the list entries
+    i = 0 # Just an incrementer for labeling the list entries
     # Print out a list of all the discovered Bluetooth Devices
     for addr, name in nearby_devices:
         print("%s. %s - %s" % (i, addr, name))
-        i = + 1
+        i =+ 1
 
     device_num = input("Please specify the number of the device you want to track: ")
 
     # extract out the useful info on the desired device for use later
     addr, name = nearby_devices[device_num][0], nearby_devices[device_num][1]
 
-print("The script will now scan for the device %s." % addr)
-print("Feel free to move near and far away from the BeagleBone to see the state change on "
-      "the LED.\nUse Ctrl+c to exit...")
+print("The script will now scan for the device %s." % (addr))
+print("Feel free to move near and far away from the BeagleBone to see the state change on the LED.\nUse Ctrl+c to exit...")
 
 # GPIO.setup(led_pin, GPIO.OUT)
 while True:
@@ -49,11 +46,11 @@ while True:
     state = bluetooth.lookup_name(addr, timeout=20)
     services = bluetooth.find_service(address=addr)
     # Flip the LED pin on or off depending on whether the device is nearby
-    if state is None and services == []:
+    if state == None and services == []:
         print("No device detected in range...")
-        # GPIO.output(led_pin, GPIO.LOW)
+        led_pin.off()
     else:
         print("Device detected!")
-        # GPIO.output(led_pin, GPIO.HIGH)
+        led_pin.on()
     # Arbitrary wait time
     time.sleep(1)
